a2a1f803f688fa61270e8b32c6a37e821fab472e
kkang2 <mallakkang@gmail.com>
2019-04-15 16:31:44 +0900

    Add files via upload

---
+++ mongo-sync/src/main/java/com/psj/db/mongosync/MongoSyncApplication.java
@@ -0,0 +1,13 @@
+package com.psj.db.mongosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoSyncApplication.class, args);
+    }
+
+}
